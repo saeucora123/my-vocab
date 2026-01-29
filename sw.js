ed97f2dec5d7ff6e2b44b189e3d5dd3c211e6cac
saeucora123 <saeucora123@gmail.com>
2026-01-29 19:23:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'my-vocab-v1';
+const urlsToCache = [
+    '/my-vocab/',
+    '/my-vocab/index.html',
+    '/my-vocab/pdf.html',
+    '/my-vocab/listening.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+});
